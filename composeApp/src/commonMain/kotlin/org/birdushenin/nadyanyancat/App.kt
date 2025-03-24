a86ca40f2f388a908765d24087c5e2ec488a84a3
ilya <uiv@coolclever.ru>
2025-03-25 00:49:29 +0300

    fix-size

--- composeApp/src/commonMain/kotlin/org/birdushenin/nadyanyancat/App.kt
+++ composeApp/src/commonMain/kotlin/org/birdushenin/nadyanyancat/App.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -121,6 +122,8 @@ fun MainMenuScreen(onStartClick: () -> Unit) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Image(
                 modifier = Modifier
+                    .height(224.dp)
+                    .width(142.dp)
                     .clip(RoundedCornerShape(42.dp))
                     .clickable {
                         onStartClick()
